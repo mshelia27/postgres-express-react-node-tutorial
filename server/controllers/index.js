4f301f5bd4d4c09850947da69cce2ee55c068851
Amos Mshelia <mshelia05@gmail.com>
2021-05-13 13:45:21 +0100

    first commit

---
+++ server/controllers/index.js
@@ -0,0 +1,12 @@
+const todos = require('./todos');
+
+module.exports = {
+  todos,
+};
+const todos = require('./todos');
+const todoItems = require('./todoitems');
+
+module.exports = {
+  todos,
+  todoItems,
+};
